0c7bcd29ed2f2a4fa371c33518d82322b5e768c0
ClaudioLeone <claleone92@gmail.com>
2023-04-19 17:27:44 +0200

    Created the searchbar and added to the site, created 'store.js' for storing the response from TMDB's API

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
-<script setup>
+<script>
+  import { store } from "./store";
+  import axios from "axios";
+  import AppSearchBar from './components/AppSearchBar.vue';
 
+  export default {
+    components: {
+      AppSearchBar
+    },
+    data() {
+      return {
+        store
+      }
+    }
+  }
 </script>
 
 <template>
-
+  <AppSearchBar />
 </template>
 
 <style scoped lang="scss">
+@use "./style/general.scss";
 
 </style>
---
+++ src/components/AppSearchBar.vue
@@ -0,0 +1,22 @@
+<script>
+    export default {
+        name: "AppSearchBar",
+        data() {
+            return {
+                store
+            }
+        }
+    }
+
+</script>
+
+<template>
+    <div class="container py-3">
+        <input type="text" name="searchBar" id="">
+        <input type="button" value="Search">
+    </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
