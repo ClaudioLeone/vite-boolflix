f42e7c3ed5f0741c270c05fbef05bef4ef6b0cfc
ClaudioLeone <claleone92@gmail.com>
2023-04-19 20:43:24 +0200

    Implemented on-page display of results from API response

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,7 @@
         if(store.query) {
           axios.get(`https://api.themoviedb.org/3/search/tv?api_key=${store.apiKey}&query=${store.query}`).then(resp => {
           console.log(resp.data.results);
+          store.films = resp.data.results;
         })
         }
       }
@@ -28,9 +29,16 @@
 
 <template>
   <AppSearchBar @search="getQuery"/>
+  <div>
+    <ul>
+      <li v-for="film in store.films"><strong>Name:</strong> <i>{{film.name}}</i> ||| <strong>Original name:</strong> <i>{{film.original_name}}</i> ||| <strong>Language:</strong> <i>{{film.original_language}}</i> ||| <strong>Vote:</strong> <i>{{ film.vote_average }}</i></li>
+    </ul>
+  </div>
 </template>
 
 <style scoped lang="scss">
 @use "./style/general.scss";
-
+li {
+  list-style: square;
+}
 </style>
