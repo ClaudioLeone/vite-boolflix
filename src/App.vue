4741223f17f00810c0ee4ba1716832f2bbe6e07c
ClaudioLeone <claleone92@gmail.com>
2023-04-21 00:13:45 +0200

    Implemented movies AND series visualization on page after query call to API

--- src/App.vue
+++ src/App.vue
@@ -15,31 +15,19 @@
       }
     },
     methods: {
-      // pushQryToArr() {
-      //   store.languages = [];
-      //   axios.get(`https://api.themoviedb.org/3/search/tv?api_key=${store.apiKey}&query=${store.query}`).then(resp => {
-      //     let totResults = resp.data.results.length;
-      //     console.log(totResults);
-
-      //     for (let i = 0; i < totResults;) {
-      //       store.languages.push(resp.data.results[i]['original_language']);
-      //       i++;
-      //     }
-      //     console.log(store.languages);
-
-      //     return store.languages;
-      //   });
-      // },
       getQuery() {
         store.query = this.store.query.trim();
         
-        if(store.query) {
-          // this.pushQryToArr();
-           
-          axios.get(`https://api.themoviedb.org/3/search/tv?api_key=${store.apiKey}&query=${store.query}`).then(resp => {
-            console.log(resp);
+        if(store.query) {           
+          axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${store.apiKey}&query=${store.query}`).then(resp => {
             store.films = resp.data.results;
-          })
+            console.log(store.films);
+          });
+
+          axios.get(`https://api.themoviedb.org/3/search/tv?api_key=${store.apiKey}&query=${store.query}`).then(resp => {
+            store.series = resp.data.results;
+            console.log(store.series);
+          });
         }
       },
     },
@@ -48,13 +36,20 @@
 
 <template>
   <AppSearchBar @search="getQuery"/>
-  <div>
+  <div class="container p-0">
     <ul>
       <li v-for="film in store.films">
-        <strong>Name:</strong> <i>{{film.name}}</i> ||| 
-        <strong>Original name:</strong> <i>{{ film.original_name }}</i> ||| 
+        <strong>Name:</strong> <i>{{ film.title }}</i> ||| 
+        <strong>Original name:</strong> <i>{{ film.original_title }}</i> ||| 
         <strong>Language:</strong> <lang-flag :iso="film.original_language" /> ||| 
         <strong>Vote:</strong> <i>{{ film.vote_average }}</i>
+      </li>
+      <hr>
+      <li v-for="singleSeries in store.series">
+        <strong>Name:</strong> <i>{{singleSeries.name}}</i> ||| 
+        <strong>Original name:</strong> <i>{{ singleSeries.original_name }}</i> ||| 
+        <strong>Language:</strong> <lang-flag :iso="singleSeries.original_language" /> ||| 
+        <strong>Vote:</strong> <i>{{ singleSeries.vote_average }}</i>
       </li>
     </ul>
   </div>
--- src/components/AppSearchBar.vue
+++ src/components/AppSearchBar.vue
@@ -14,11 +14,27 @@
 
 <template>
     <div class="container py-3">
-        <input type="text" placeholder="Search a movie..." v-model="store.query" @keyup.enter="$emit('search')">
-        <input type="button" value="Search" @click="$emit('search')">
+        <input class="searchbar" type="text" placeholder="Search a movie/series" v-model="store.query" @keyup.enter="$emit('search')">
+        <input class="searchbar-btn" type="button" value="Search" @click="$emit('search')">
     </div>
 </template>
 
 <style scoped lang="scss">
+.searchbar {
+    width: 20rem;
+    border: 1px solid lightgrey;
+    border-radius: 3px;
+    background-color: wheat;
+    padding: 0 5px;
+}
 
+.searchbar-btn {
+    border: 1px solid lightgrey;
+    border-radius: 3px;
+    background-color: wheat;
+    color: #2b2b2b;
+    font-weight: bold;
+    padding: 0 5px;
+    margin-left: 5px;
+}
 </style>
